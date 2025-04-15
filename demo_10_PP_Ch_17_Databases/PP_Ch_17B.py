d3508571c699081e30e74611c83f4a87911eb849
Lee Morin <42477710+LeeMorinUCF@users.noreply.github.com>
2025-04-15 13:21:20 -0400

    Revised demo content for databases

--- demo_10_PP_Ch_17_Databases/PP_Ch_17B.py
+++ demo_10_PP_Ch_17_Databases/PP_Ch_17B.py
@@ -2,7 +2,7 @@
 """
 ##################################################
 # 
-# QMB 3311: Python for Business Analytics
+# QMB 6315: Python for Business Analytics
 # 
 # Using Databases with SQLite3
 # 
@@ -12,7 +12,7 @@
 # College of Business
 # University of Central Florida
 # 
-# April 3, 2021
+# April 12, 2025
 # 
 # Chapter 17: Databases
 # Part B: Advanced Features
@@ -40,14 +40,26 @@ import sqlite3
 ##################################################
 
 
+
 # Find out the current directory.
 os.getcwd()
+
+# Get the path where you saved this script.
+# This only works when you run the entire script (with the green "Play" button or F5 key).
+print(os.path.dirname(os.path.realpath(__file__)))
+# It might be comverted to lower case, but it gives you an idea of the text of the path. 
+# You could copy it directly or type it yourself, using your spelling conventions. 
+
 # Change to a new directory.
-drive_path = 'C:\\Users\\le279259\\OneDrive - University of Central Florida\\Documents\\'
-git_path = 'GitHub\\QMB3311S22\\'
-os.chdir(drive_path + git_path + 'demo_24_PP_Ch_17_Databases')
+
+# You could set it directly from the location of this file
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
 # Check that the change was successful.
 os.getcwd()
+# I got lower case output, even though my folders have some upper case letters.
+# But anyway, it works.
+
 
 
 ##################################################
